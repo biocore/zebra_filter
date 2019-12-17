e551e724fe56c41370479a901f25dd64c7adbf76
Stephen Wandro <swandro@swandros-MacBook-Pro.local>
2019-12-16 17:04:47 -0800

    Made pipeline into Click. Also only refers to one database file now.

--- full_pipeline_python.py
+++ full_pipeline_python.py
@@ -1,75 +1,21 @@
 #Zebra filter full pipeline
 
-from glob import glob
 import pandas as pd
 from collections import defaultdict
 import re
 from sys import argv
 from os import path
+import click
 
-#Input1 : sam file list
-#Input2 : output name
+@click.command()
+@click.option('--sam','-s', required=True, help="List of sam files 1 file per line.")
+@click.option('--output','-o', required=True, help='Output file name for list of coverages.')
+@click.option('--database', default="database/wol_zebra_db.tsv", help='List of ncbi sequence ids to remove from sam.')
 
-#######################
-#Check for input files#
-#######################
-try:
-    sam_list = argv[1]
-except IndexError:
-    print("No input file list provided.")
-    print("Input 1: txt file with list of sam files, 1 file per line.")
-    print("Input 2: output file name.")
-    print("Exiting.")
-    exit()
-
-try:
-    out_name = argv[2]
-except IndexError:
-    print("No output name provided.")
-    print("Input 1: txt file with list of sam files, 1 file per line.")
-    print("Input 2: output file name.")
-    print("Exiting.")
-    exit()
-
-if not path.exists(sam_list):
-    print("Cannot locate input file list {}".format(sam_list))
-    print("Exiting.")
-    exit()
-
-any_missing = False
-with open(sam_list,'r') as openfile:
-    for line in openfile:
-        if not path.exists(line.strip()):
-            print("Cannot locate input file: {}".format(line))
-            any_missing=True
-
-##############
-#Helper files#
-##############
-# nucl2tid: link ncbi id to taxid
-nucl2tid_file="/home/swandro/databases/nucl2tid.txt"
-
-# nucl2lineage: link taxid to taxonomy
-taxid2taxonomy_file="/home/swandro/databases/taxid2taxonomy.txt"
-
-# taxid2length: link taxid to length
-taxid_length_file="/home/swandro/databases/taxid2length.txt"
-
-#Ensure all helper files exist
-for file in [nucl2tid_file,taxid2taxonomy_file,taxid_length_file]:
-    if not path.exists(file):
-        print("Cannot locate helper file {}".format(file))
-        any_missing=True
-
-if any_missing:
-    print("Exiting.")
-    exit()
-
-#####################
-#Calculate coverages#
-#####################
-
-def make_contig_dict():
+def calculate_coverages(sam, output, database):
+    ###################################
+    #Calculate coverage of each contig#
+    ###################################
     contig_dict = defaultdict(set)
     with open(sam_list,'r') as open_list_file:
         for samfile in open_list_file:
@@ -85,62 +31,51 @@ def make_contig_dict():
                     #Add range to contig_dict
                     for i in range(location,location+length):
                         contig_dict[contig].add(i)
-    return(contig_dict)
 
 
-###################################
-#Get information from helper files#
-###################################
-ncbi_taxid_dict = dict()
-taxid_length_dict = dict()
-taxid_taxonomy_dict = dict()
+    ###################################
+    #Get information from database#
+    ###################################
+    ncbi_taxid_dict = dict()
+    taxid_length_dict = dict()
+    taxid_taxonomy_dict = dict()
 
-with open(nucl2tid_file, 'r') as openfile:
-    for line in openfile:
-        ncbi = line.split()[0].strip()
-        taxid = line.split()[1].strip()
-        ncbi_taxid_dict[ncbi] = taxid
+    database_df = pd.read_csv(database, sep='\t')
+    for i,line in database_df.iterrows():
+        ncbi_taxid_dict[line.ncbi] = line.taxid
+        taxid_length_dict[line.ncbi] = int(line.total_length)
+        taxid_taxonomy_dict[line.taxid] = line.taxonomy
 
-with open(taxid_length_file, 'r') as openfile:
-    openfile.readline()
-    for line in openfile:
-        ncbi = line.split()[0].strip()
-        length = line.split()[1].strip()
-        taxid_length_dict[ncbi] = int(length)
+    ###funcitons to use helper information
+    def get_taxid(x):
+        return(ncbi_taxid_dict[x["ncbi_id"]])
+    def get_taxonomy(x):
+        return(taxid_taxonomy_dict[str(x.name)])
+    def get_length(x):
+        return(taxid_length_dict[str(x.name)])
 
-with open(taxid2taxonomy_file, 'r') as openfile:
-    for line in openfile:
-        taxid = line.split()[0].strip()
-        taxonomy = line.split()[1].strip()
-        taxid_taxonomy_dict[taxid] = taxonomy
+    #####################
+    #Calculate coverages#
+    #####################
+    #Make dataframe from dicitonary of coverages of each contig
+    cov = pd.DataFrame({"ncbi_id":list(contig_dict.keys()),
+                        "covered_length" : [len(x) for x in contig_dict.values()] } )
+    #Merge contigs by taxid
+    cov["taxid"] = cov.apply(func=get_taxid, axis=1)
+    cov = cov.groupby("taxid").agg({"covered_length":sum})
 
-###funcitons to use helper information
-def get_taxid(x):
-    return(ncbi_taxid_dict[x["ncbi_id"]])
-def get_taxonomy(x):
-    return(taxid_taxonomy_dict[str(x.name)])
-def get_length(x):
-    return(taxid_length_dict[str(x.name)])
+    #Add total length of each taxid and calculate coverage percent
+    cov["total_length"] = cov.apply(func=get_length, axis=1)
+    cov["coverage_pct"] = cov.apply(func= lambda x : x["covered_length"]/x["total_length"], axis=1)
 
-#####################
-#Calculate coverages#
-#####################
-#Make dataframe from dicitonary of coverages of each contig
-cov = pd.DataFrame({"ncbi_id":list(contig_dict.keys()),
-                    "covered_length" : [len(x) for x in contig_dict.values()] } )
-#Merge contigs by taxid
-cov["taxid"] = cov.apply(func=get_taxid, axis=1)
-cov = cov.groupby("taxid").agg({"covered_length":sum})
+    #Add taxonomy and group taxids by taxonomy name. Take the max covered taxid for each taxonomy
+    cov["taxonomy"] = cov.apply(func=get_taxonomy, axis=1)
+    cov = cov.groupby("taxonomy").agg({"covered_length":"max","coverage_pct":"max"}).sort_values("coverage_pct",ascending=False)
 
-#Add total length of each taxid and calculate coverage percent
-cov["total_length"] = cov.apply(func=get_length, axis=1)
-cov["coverage_pct"] = cov.apply(func= lambda x : x["covered_length"]/x["total_length"], axis=1)
+    ##############
+    #Write output#
+    ##############
+    cov.to_csv(output, sep='\t')
 
-#Add taxonomy and group taxids by taxonomy name. Take the max covered taxid for each taxonomy
-cov["taxonomy"] = cov.apply(func=get_taxonomy, axis=1)
-cov = cov.groupby("taxonomy").agg({"covered_length":"max","coverage_pct":"max"}).sort_values("coverage_pct",ascending=False)
-
-##############
-#Write output#
-##############
-cov.to_csv(out_name, sep='\t')
+if __name__ == "__main__":
+    calculate_coverages()
